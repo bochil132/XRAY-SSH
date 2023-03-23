133e60603504ddd43f63345058b0cb3752ebcfed
bochil132 <117184347+bochil132@users.noreply.github.com>
2023-03-24 06:08:24 +0700

    Update cloudflare-pointing.sh

--- menu/cloudflare-pointing.sh
+++ menu/cloudflare-pointing.sh
@@ -121,7 +121,7 @@ fi
          -H "X-Auth-Email: ${CF_ID}" \
          -H "X-Auth-Key: ${CF_KEY}" \
          -H "Content-Type: application/json" \
-         --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+         --data '{"type":"A","name":"'${sub}.stn-cloud.my.id'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
     fi
     
     RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
