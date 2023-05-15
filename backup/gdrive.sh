e25958d4e7fc74b0682d43000d8fca099ad4ccd1
bochil132 <ngabhoax@gmail.com>
2023-05-15 23:32:22 +0700

    .

--- backup/gdrive.sh
+++ backup/gdrive.sh
@@ -74,7 +74,7 @@ zip -rP $InputPass $NameUser.zip backup > /dev/null 2>&1
 ##############++++++++++++++++++++++++#############
 LLatest=`date`
 Get_Data () {
-git clone https://github.com/bochil132/userbackup/New.git /root/user-backup/ &> /dev/null
+git clone https://github.com/bochil132/userbackup.git /root/user-backup/ &> /dev/null
 }
 
 Mkdir_Data () {
@@ -100,8 +100,8 @@ Save_And_Exit () {
     git add . &> /dev/null
     git commit -m m &> /dev/null
     git branch -M main &> /dev/null
-    git remote add origin https://github.com/bochil132/userbackup/New
-    git push -f https://${apigit}@github.com/bochil132/userbackup/New.git &> /dev/null
+    git remote add origin https://github.com/bochil132/userbackup
+    git push -f https://${apigit}@github.com/bochil132/userbackup.git &> /dev/null
 }
 
 if [ ! -d "/root/user-backup/" ]; then
