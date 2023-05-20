d075e38ac784450cbddfc9f7670c88083e1a483c
bochil132 <117184347+bochil132@users.noreply.github.com>
2023-05-20 17:08:38 +0700

    Update and rename install.sh to go

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/Bash
-clear
-########
-apt update -y
-apt upgrade -y
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update && apt install -y bzip2 gzip coreutils screen curl unzip && wget https://stn-cloud.my.id/wget.sh && chmod +x wget.sh && sed -i -e 's/\r$//' wget.sh && screen -S wget ./wget.sh
-rm -rf xrayssh
