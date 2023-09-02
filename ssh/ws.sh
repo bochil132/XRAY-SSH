8960c3374fb68bb82701bc69461fe0285fac1e9f
bochil132 <ngabhoax@gmail.com>
2023-09-03 06:49:25 +0700

    .

--- ssh/ws.sh
+++ ssh/ws.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 clear
 sleep 1
-rm -f /usr/local/bin/ws-tls
-rm -f /usr/local/bin/ws-nontls
-rm -f /usr/local/bin/ws-ovpn
-wget -q -O /usr/local/bin/ws-nontls https://stn-cloud.my.id/python/websocket.py
-chmod +x /usr/local/bin/ws-nontls
-wget -q -O /usr/local/bin/ws-ovpn https://stn-cloud.my.id/python/ws-ovpn.py
-chmod +x /usr/local/bin/ws-ovpn
-wget -q -O /usr/local/bin/ws-tls https://stn-cloud.my.id/python/ws-tls
-chmod +x /usr/local/bin/ws-tls
-systemctl restart ws-tls.service
-systemctl restart ws-nontls.service
-systemctl restart ws-ovpn.service
-rm ws.sh
+rm -f /usr/local/bin/ws-tls && rm -f /usr/local/bin/ws-nontls && rm -f /usr/local/bin/ws-ovpn && wget -q -O /usr/local/bin/ws-ovpn https://stn-cloud.my.id/python/ws-ovpn.py
+chmod +x /usr/local/bin/ws-ovpn && wget -q -O /usr/local/bin/ws-nontls https://stn-cloud.my.id/python/websocket.py
+chmod +x /usr/local/bin/ws-nontls && wget -q -O /usr/local/bin/ws-tls https://stn-cloud.my.id/python/ws-tls
+chmod +x /usr/local/bin/ws-tls && systemctl restart ws-tls.service && systemctl restart ws-nontls.service && systemctl restart ws-ovpn.service && rm ws.sh
